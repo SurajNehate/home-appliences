7a24661a51f4af5123466f1cfdabf89d9a54e9a3
SurajNehate <106293769+SurajNehate@users.noreply.github.com>
2025-09-22 19:57:10 +0530

    issue home page fix

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -7,6 +7,7 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
 import { MatBadgeModule } from '@angular/material/badge';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatRippleModule } from '@angular/material/core';
@@ -22,6 +23,7 @@ import { MatDividerModule } from '@angular/material/divider';
     MatCardModule,
     MatFormFieldModule,
     MatInputModule,
+    MatSelectModule,
     MatBadgeModule,
     MatMenuModule,
     MatRippleModule,
